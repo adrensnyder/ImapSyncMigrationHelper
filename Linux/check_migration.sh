eaabb54287f57b675e25a07b5b5d3019b1333cef
AdrenSnyder <andrea.cariddi@outlook.com>
2026-02-19 10:23:56 +0100

    Refactor check_migration.sh for improved readability

--- Linux/check_migration.sh
+++ Linux/check_migration.sh
@@ -24,83 +24,91 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 ###################################################################
 
+#!/bin/bash
+set -u
 
-#!/bin/sh
-
-LOGPATH=$1
+LOGPATH=${1:-}
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-if [[ "X$LOGPATH" == "X" ]]; then
-    echo "Enter a valid path for migration logs"
-    echo "Ex. /var/log/imapsync/job/data"
-    exit
+if [[ -z "$LOGPATH" ]]; then
+  echo "Enter a valid path for migration logs"
+  echo "Ex. /var/log/imapsync/job/data"
+  exit 1
 fi
 
-if [ ! -d $LOGPATH ]; then
-        echo "Enter a valid path for migration logs"
-        echo "Ex. /var/log/imapsync/job/data"
-    exit
+if [[ ! -d "$LOGPATH" ]]; then
+  echo "Enter a valid path for migration logs"
+  echo "Ex. /var/log/imapsync/job/data"
+  exit 1
 fi
 
-LIST=$(ls -1 $LOGPATH/*%*)
+# --- Build LASTLOGS safely (works with absolute paths) ---
+shopt -s nullglob
 
-LIST_NEW=""
-LIST_UNIQUE=""
-for file in $LIST; do
-    EMAIL=`echo $file | awk -F '%' '{ print $1 }'`
-    LIST_NEW="$LIST_NEW $EMAIL"
+FILES=( "$LOGPATH"/*%* )
+if [[ ${#FILES[@]} -eq 0 ]]; then
+  echo "No log files matching *%* found in: $LOGPATH"
+  exit 0
+fi
+
+PREFIXES=()
+for f in "${FILES[@]}"; do
+  base="${f##*/}"        # filename only
+  prefix="${base%%\%*}"  # part before first %
+  PREFIXES+=( "$prefix" )
 done
 
-LIST_UNIQUE=`echo $LIST_NEW | tr ' ' '\n' | sort -u`
+mapfile -t LIST_UNIQUE < <(printf "%s\n" "${PREFIXES[@]}" | sort -u)
 
 MESSAGESOK_COUNT=0
 GOOD_COUNT=0
 NOTSTRICT_COUNT=0
 NOTFINISHED_COUNT=0
+COUNT_LIST=0
 
-LASTLOGS=()  # inizializza array
+LASTLOGS=()
 echo ""
 echo -e "- ${RED}File list${NC}"
 
-for file in $LIST_UNIQUE; do
-    ((COUNT_LIST++))
-    LASTLOG=$(ls -1 -r "$LOGPATH/$file%"* | head -n1)
-    [[ -n "$LASTLOG" ]] && LASTLOGS+=("$LASTLOG")
-
-    echo "$file -> $LASTLOG"
+for p in "${LIST_UNIQUE[@]}"; do
+  ((COUNT_LIST++))
+  # pick latest by filename ordering (your logs embed datetime in name)
+  LASTLOG=$(ls -1 -r -- "$LOGPATH/$p%"* 2>/dev/null | head -n1)
+  [[ -n "$LASTLOG" ]] && LASTLOGS+=( "$LASTLOG" )
+  echo "$p -> $LASTLOG"
 done
 
+# --- Stats over last logs ---
 for file in "${LASTLOGS[@]}"; do
+  MESSAGESOK=$(grep -a "Messages found in host1 not in host2" "$file" | grep ": 0 messages" | wc -l)
+  GOOD=$(grep -a "The sync looks good" "$file" | wc -l)
+  NOTSTRICT=$(grep -a "The sync is not strict" "$file" | wc -l)
+  NOTFINISHED=$(grep -a "The sync is not finished" "$file" | wc -l)
 
-    MESSAGESOK=$(grep -a "Messages found in host1 not in host2" "$file"|grep ": 0 messages" |wc -l)
-    GOOD=$(grep -a "The sync looks good" "$file"|wc -l)
-    NOTSTRICT=$(grep -a "The sync is not strict" "$file"| wc -l)
-    NOTFINISHED=$(grep -a "The sync is not finished" "$file"|wc -l)
+  if [[ "$MESSAGESOK" -gt 0 ]]; then
+    ((MESSAGESOK_COUNT++))
+  fi
 
-        if [ "$MESSAGESOK" -gt "0" ]; then
-                let "MESSAGESOK_COUNT=MESSAGESOK_COUNT+1"
-        fi
+  if [[ "$GOOD" -gt 0 ]]; then
+    ((GOOD_COUNT++))
+  fi
 
-    if [ "$GOOD" -gt "0" ]; then
-        let "GOOD_COUNT=GOOD_COUNT+1"
-    fi
+  if [[ "$NOTFINISHED" -gt 0 ]]; then
+    ((NOTFINISHED_COUNT++))
+  fi
 
-    if [ "$NOTFINISHED" -gt "0" ]; then
-        let "NOTFINISHED_COUNT=NOTFINISHED_COUNT+1"
-    fi
-
-    if [[ "$NOTSTRICT" -gt "0" && "$GOOD" -eq "0" && "$NOTFINISHED" -eq "0" ]]; then
-        let "NOTSTRICT_COUNT=NOTSTRICT_COUNT+1"
-    fi
-
+  if [[ "$NOTSTRICT" -gt 0 && "$GOOD" -eq 0 && "$NOTFINISHED" -eq 0 ]]; then
+    ((NOTSTRICT_COUNT++))
+  fi
 done
 
 ALL_GOOD_NOTFINISHED=0
 for file in "${LASTLOGS[@]}"; do
-    GOOD_NOTFINISHED=`grep -a "Exiting with return value" $file |wc -l`
-    let "ALL_GOOD_NOTFINISHED=ALL_GOOD_NOTFINISHED+GOOD_NOTFINISHED"
+  GOOD_NOTFINISHED=$(grep -a "Exiting with return value" "$file" | wc -l)
+  ((ALL_GOOD_NOTFINISHED += GOOD_NOTFINISHED))
 done
+
 echo ""
 echo -e "- ${RED}General Stats${NC}"
 echo "Total emails in check: $COUNT_LIST"
@@ -115,80 +123,70 @@ echo ""
 echo -e "${RED}- CHECK Status${NC}"
 
 for file in "${LASTLOGS[@]}"; do
-        GREP_RESULT=""
-        GREP_RESULT=`grep -a --colour -iTHn -E 'Exiting with return value' "$file" |wc -l`
-        if [ "$GREP_RESULT" -eq "0" ]; then
-                PS_TEST=`ps auxwf |grep -a imapsync |grep $(basename "$file")|wc -l`
-                if [ "$PS_TEST" -gt "0" ]; then
-                        echo "--> "$file" Imapsync process is running"
-                else
-                        echo "--> "$file" Not completed. Probably terminated cause of an 'Out of memory' error. Try to limit attachments size with --maxsize 150_000_000"
-                fi
-        else
-                GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -E 'Exiting with return value' "$file"
-        fi
-
+  GREP_RESULT=$(grep -a --colour -iTHn -E 'Exiting with return value' "$file" | wc -l)
+  if [[ "$GREP_RESULT" -eq 0 ]]; then
+    PS_TEST=$(ps auxwf | grep -a imapsync | grep "$(basename "$file")" | wc -l)
+    if [[ "$PS_TEST" -gt 0 ]]; then
+      echo "--> $file Imapsync process is running"
+    else
+      echo "--> $file Not completed. Probably terminated cause of an 'Out of memory' error. Try to limit attachments size with --maxsize 150_000_000"
+    fi
+  else
+    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -E 'Exiting with return value' "$file"
+  fi
 done
 
 echo ""
 echo -e "${RED}Emails not completed:${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "The sync is not finished" "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "The sync is not finished" "$file"
 done
 
 echo ""
 echo -e "${RED}- CHECK ERROR LOGIN${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "Error login" "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "Error login" "$file"
 done
 
 echo ""
 echo -e "${RED}- CHECK BAD USER (O365)${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "bad user" "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "bad user" "$file"
 done
 
 echo ""
 echo -e "${RED}- CHECK EX_OK${NC}"
 EXOK_COUNT=0
-
 for file in "${LASTLOGS[@]}"; do
-    EXOK=`grep -a -i "EX_OK" "$file"|wc -l`
-    if [ "$EXOK" -gt "0" ]; then
-        let "EXOK_COUNT=EXOK_COUNT+1"
-    fi
+  EXOK=$(grep -a -i "EX_OK" "$file" | wc -l)
+  if [[ "$EXOK" -gt 0 ]]; then
+    ((EXOK_COUNT++))
+  fi
 done
-echo $EXOK_COUNT
+echo "$EXOK_COUNT"
 
 echo ""
 echo -e "${RED}- CHECK EXIT_ERR_APPEND${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "EXIT_ERR" "$file"
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "Maximum number of errors.*reached|reached.*Maximum number of errors" "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "EXIT_ERR" "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn "Maximum number of errors.*reached|reached.*Maximum number of errors" "$file"
 done
 
 echo ""
 echo -e "${RED}- CHECK skipped${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -E 'msg.*skipped|skipped.*msg' "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -E 'msg.*skipped|skipped.*msg' "$file"
 done
 
 echo ""
 echo -e "${RED}- CHECK Err${NC}"
-
 for file in "${LASTLOGS[@]}"; do
-    #GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -E 'msg.*Err |Err .*msg' "$file"
-    GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -P '^Err ' "$file"
+  GREP_COLORS='fn=01;34:ln=01;32:mt=01;35' grep -a --colour -iTHn -P '^Err ' "$file"
 done
 
 echo ""
-echo -e "${RED}- CHECK Err (parsed)${NC}"
-echo "LOG|LINE|PRE_ERR|ERRTAG|POST|subject|date|size|flags"
+echo -e "${RED}- CHECK Err (split LOG path/account + extract fields)${NC}"
+echo "log_path|account|log_dt|LINE|PRE_ERR|ERRTAG|subject|date|size|flags|POST"
 
 for file in "${LASTLOGS[@]}"; do
   GREP_COLORS='' grep -a --color=never -iHn 'Err ' "$file" \
@@ -204,9 +202,24 @@ for file in "${LASTLOGS[@]}"; do
           function trim(s){ sub(/^[[:space:]]+/, "", s); sub(/[[:space:]]+$/, "", s); return s }
 
           {
-            # Ricostruisci POST se contiene altri "|"
             post = $5
             for (i=6; i<=NF; i++) post = post "|" $i
+
+            log_full = $1
+            n = split(log_full, L, /%20/)
+            log_name = L[1]
+            log_dt   = (n >= 2 ? L[2] : "")
+
+            log_name = trim(log_name)
+            log_dt   = trim(log_dt)
+
+            p = log_name
+            gsub(/\/+$/, "", p)
+            last = p
+            sub(/^.*\//, "", last)
+            path = p
+            sub(/\/[^\/]*$/, "", path)
+            if (path == p) path = ""
 
             subject=""; date=""; size=""; flags=""
 
@@ -220,7 +233,6 @@ for file in "${LASTLOGS[@]}"; do
             if (hz) size    = c[1]
             if (hf) flags   = d[1]
 
-            # Pulisci da doppi apici e apici singoli (solo nei campi estratti)
             gsub(/["'\''"]/, "", subject)
             gsub(/["'\''"]/, "", date)
             gsub(/["'\''"]/, "", size)
@@ -231,10 +243,8 @@ for file in "${LASTLOGS[@]}"; do
             size    = trim(size)
             flags   = trim(flags)
 
-            # POST resta intatto
-            print $1 "|" $2 "|" $3 "|" $4 "|" subject "|" date "|" size "|" flags "|" post
+            print path "|" last "|" log_dt "|" $2 "|" $3 "|" $4 "|" subject "|" date "|" size "|" flags "|" post
           }
         '
     done
 done
-
